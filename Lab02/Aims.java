c446cacf929061fa65486b5e1a3b4fcfc16b4d75
KenChim <giaanhnp@gmail.com>
2026-04-01 11:19:15 +0700

    Added photos and asta file

--- Lab02/Aims.java
+++ Lab02/Aims.java
@@ -7,16 +7,16 @@ public class Aims {
     Cart anOrder = new Cart();
 
     //Create new dvd objects and add them to the cart
-    DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-            "Animation", "Roger Allers", 87, 19.95f);
+    DigitalVideoDisc dvd1 = new DigitalVideoDisc("5cm/s",
+            "Anime", "Makoto Shinkai", 97, 19.95f);
     anOrder.addDigitalVideoDisc(dvd1);
 
-    DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+    DigitalVideoDisc dvd2 = new DigitalVideoDisc("TenSura",
             "Science Fiction", "George Lucas", 87, 24.95f);
     anOrder.addDigitalVideoDisc(dvd2);
 
-    DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-            "Animation", 18.99f);
+    DigitalVideoDisc dvd3 = new DigitalVideoDisc("Asagao to Kase-san",
+            "Yuri", 18.99f);
     anOrder.addDigitalVideoDisc(dvd3);
 
     //print total cost of the items in the cart
