0a5f7c5639dbe2797a72a148f76e4d881487fea4
KenChim <giaanhnp@gmail.com>
2026-04-02 11:47:54 +0700

    Added sufficient photos, refactored the folders, added Answers.txt and mindmap - finishing Lab2

--- Lab02/DigitalVideoDisc.java
+++ Lab02/DigitalVideoDisc.java
@@ -11,14 +11,11 @@ public class DigitalVideoDisc{
         this.category = category;
         this.cost = cost;
     }
-    public DigitalVideoDisc(String title, String category) {
-        this.title = title;
-        this.category = category;
-    }
-    public DigitalVideoDisc(String title, String category, String director) {
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
+        this.cost = cost;
     }
     public DigitalVideoDisc(String title) {
         super();
@@ -34,31 +31,16 @@ public class DigitalVideoDisc{
     public String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
     public String getCategory() {
         return category;
-    }
-    public void setCategory(String category) {
-        this.category = category;
     }
     public String getDirector() {
         return director;
     }
-    public void setDirector(String director) {
-        this.director = director;
-    }
     public int getLength() {
         return length;
     }
-    public void setLength(int length) {
-        this.length = length;
-    }
     public float getCost() {
         return cost;
-    }
-    public void setCost(float cost) {
-        this.cost = cost;
     }
 }
